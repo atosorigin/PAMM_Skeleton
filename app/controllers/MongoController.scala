ddf6864957623e938eaf7933d39da10ef5dd0ba1
Mark Fearnley <mark.fearnley@atos.net>
2016-05-11 10:57:27 +0100

    Added error handling for missing database.

--- app/controllers/MongoController.scala
+++ app/controllers/MongoController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import com.mongodb.MongoException
 import play.api.mvc.{Action, Controller}
 import com.mongodb.casbah.Imports._
 
@@ -18,27 +19,31 @@ object MongoController extends Controller {
     */
   def helloMongo = Action {
 
-    val collection = MongoClient()("pamm_skeleton")("hello_play")
+    try {
+      val collection = MongoClient()("pamm_skeleton")("hello_play")
 
-    // Get a list of all current counts saved to the database
-    val q = countTag $exists true
-    val counts = for (x <- collection.find(q)) yield x
+      // Get a list of all current counts saved to the database
+      val q = countTag $exists true
+      val counts = for (x <- collection.find(q)) yield x
 
-    // If a count already exists, get the first one (for simplicity) and increment, otherwise the current count is 1
-    val count = if (counts.isEmpty) 1 else counts.next().getAs[Int](countTag).get + 1
+      // If a count already exists, get the first one (for simplicity) and increment, otherwise the current count is 1
+      val count = if (counts.isEmpty) 1 else counts.next().getAs[Int](countTag).get + 1
 
-    if (counts.isEmpty) {
-      // If there are no counts, save a new count object
-      val newCount = MongoDBObject(countTag -> count)
-      collection += newCount
-    } else {
-      // Otherwise update the existing count object
-      val newCount = MongoDBObject(countTag -> count)
-      collection.update(q, newCount)
+      if (counts.isEmpty) {
+        // If there are no counts, save a new count object
+        val newCount = MongoDBObject(countTag -> count)
+        collection += newCount
+      } else {
+        // Otherwise update the existing count object
+        val newCount = MongoDBObject(countTag -> count)
+        collection.update(q, newCount)
+      }
+
+      // Pass back the count
+      Ok(String.valueOf(count))
+    } catch {
+      case ex: MongoException => InternalServerError("Database not found.")
     }
-
-    // Pass back the count
-    Ok(String.valueOf(count))
   }
 
 }
