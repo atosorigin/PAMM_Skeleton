68cf05980e68840c648804feeb6fad6e498c3083
Mark Fearnley <tractor.unit@gmail.com>
2016-07-21 11:40:32 +0100

    HelloWorld service now passes in the correct authorization header.

--- public/script/services/helloWorldService.js
+++ public/script/services/helloWorldService.js
@@ -3,8 +3,10 @@
 var module = angular.module('pammSkeleton.services');
 
 // Example service which makes a GET call to our Play service, and returns the data as-is.
-module.factory('helloWorldService', ["$http", function($http) {
-    return $http.get("/ws/hello")
+module.factory('helloWorldService', ["$http", "$rootScope", function($http, $rootScope) {
+    return $http.get("/ws/hello", {
+        headers: { 'Authorization': 'Bearer ' + window.btoa($rootScope.username + ':' + $rootScope.authToken) }
+    })
         .success(function(data) {
           return data;
         })
